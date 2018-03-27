475fb1446efaf8e5c162a0da2a8c0f04e35b391d
ravinder <jajoriaravinder99@gmail.com>
2018-03-27 18:20:47 +0530

    Added functionality to filter file arguments

--- src/wc/util.clj
+++ src/wc/util.clj
@@ -3,3 +3,10 @@
 (defn isOption
   [arg]
   (.startsWith arg "-"))
+
+(defn filter-not-option
+  [elements]
+  (filter
+    (fn [x] (not (.startsWith x "-")))
+    elements))
+    
--- test/wc/util_test.clj
+++ test/wc/util_test.clj
@@ -2,10 +2,14 @@
   (:require [clojure.test :refer :all]
             [wc.util :refer :all]))
 
-(deftest a-test
+(deftest is-option-test
   (testing "should pass when argument is an option"
     (is (isOption "-abc"))))
 
-(deftest a-test
+(deftest is-option-test
   (testing "should fail when argument is not an option"
     (is (not (isOption "abc")))))
+
+(deftest filter-not-option-test
+  (testing "should return list of arguments which are not option"
+    (is (= (filter-not-option '("-abc" "abc" "-a" "a" "b" "-b")) '("abc" "a" "b")))))
